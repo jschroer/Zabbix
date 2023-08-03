69c53801439d9a4f176e7c042e44b34077736942
Jochen Schrör <jschroer@users.noreply.github.com>
2023-08-03 18:37:05 +0200

    Update autoregister_update_interfaces.py
    change for login in newer pyzabbix lib

--- autoregistration/autoregister_update_interfaces.py
+++ autoregistration/autoregister_update_interfaces.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Set host inventory to automatic.
+Set host interfaces to defined IP
 """
 
 from pyzabbix import ZabbixAPI
@@ -17,11 +17,9 @@ log.addHandler(stream)
 #log.setLevel(logging.DEBUG)
 log.setLevel(logging.ERROR)
 
-
+# login into Zabbix API
 zapi = ZabbixAPI(ZABBIX_SERVER)
-
-# Enable HTTP auth
-zapi.session.auth = ("http user", "http password")
+zapi.login(API_USER,API_PASSWORT)
 
 # Disable SSL certificate verification
 zapi.session.verify = False
@@ -29,24 +27,17 @@ zapi.session.verify = False
 # Specify a timeout (in seconds)
 zapi.timeout = 5.1
 
-# Login to the Zabbix API
-zapi.login(API_USER,API_PASSWORT)
-
-# You can also authenticate using an API token instead of user/pass with Zabbix >= 5.4
-# zapi.login(api_token='xxxxx')
-
-
 #search the host we will modify
-host = zapi.host.get(filter={'host':client_hostname},selectInterfaces="extend",limit=4 )
+host = zapi.host.get(filter={'host':client_hostname},selectInterfaces="extend",limit=4)
 
 # get the hostID into separate variable
 hostIdValue = host[0]['hostid']
 
 # loop over interfaces and update ip or delete them
 for interface in host[0]['interfaces']:
-   if interface['main'] == "1":     # if it is a default interface than change ip
-     interface['ip'] = client_ip
-     zapi.hostinterface.update(interface);
-   else:
-     zapi.hostinterface.delete(interface['interfaceid']) # if it is a additional interface delete it
+    if interface['main'] == "1":     # if it is a default interface than change ip
+      interface['ip'] = client_ip
+      zapi.hostinterface.update(interface);
+    else:
+      zapi.hostinterface.delete(interface['interfaceid']) # if it is a additional interface delete it
 
